f5050ac47d21df1cd0815a86ba08de8c413b8dde
waldos09 <legos567@gmail.com>
2020-05-15 21:43:43 -0500

    Primera pagina sin login correcto

---
+++ js/login.js
@@ -0,0 +1,16 @@
+function login() {
+    // body...
+    let name = document.getElementById('user').value;
+    let password = document.getElementById('pass').value;
+
+    if (name = "Alma") {
+        if (password = "1310") {
+            alert("Hola esta funcion no srive x el momento nose x q");
+            window.location = "main1.html";
+            window.HTMLDocument = "main1.html";
+            window.HTMLLinkElement = "main1.html";
+        }
+    } else {
+        alert("Ingrese los datos correctos");
+    }
+}
